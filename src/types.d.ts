ef413b93bd4c15c7a4a7b8e644c3fd79eec9584d
David Barragán Merino <david.barragan@kaleidos.net>
2021-04-14 17:22:09 +0200

    Repo freeze

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2014-2020 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
 declare module 'taiga-html-editor/packages/ckeditor5-build-classic/build/ckeditor' {
   const content: any;
 	export default content;
